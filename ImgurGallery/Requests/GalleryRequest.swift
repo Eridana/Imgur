8c5089032cfcb32623b2b1a49787f9836e2733af
Jenya <evgenia.mikhailova@cuciniale.eu>
2018-10-11 17:34:24 +0200

    Covers request moved to service

--- ImgurGallery/Requests/GalleryRequest.swift
+++ ImgurGallery/Requests/GalleryRequest.swift
@@ -46,28 +46,7 @@ class GalleryRequest {
             if let data = data as? Data {
                 do {
                     let decoded = try JSONDecoder().decode(IMGalleryRequestResult.self, from: data)
-                    
-                    // request cover images info
-                    guard let galleries = decoded.galleries else {
-                        completion(nil)
-                        return
-                    }
-                    
-                    let dispatchGroup = DispatchGroup()
-                    for gallery in galleries {
-                        if let coverId = gallery.coverImageId {
-                            dispatchGroup.enter()
-                            self.imageRequest.imageInfo(for: coverId, completion: { (cover) -> (Void) in
-                                gallery.coverImage = cover
-                                dispatchGroup.leave()
-                            })
-                        }
-                    }
-                    
-                    dispatchGroup.notify(queue: .main) {
-                        completion(decoded.galleries)
-                    }
-                    
+                    completion(decoded.galleries)
                 } catch {
                     print(error)
                     completion(nil)
--- ImgurGallery/Services/GalleriesService.swift
+++ ImgurGallery/Services/GalleriesService.swift
@@ -11,12 +11,28 @@ import Foundation
 class GalleriesService {
     
     private var request = GalleryRequest()
+    private let imageRequest = ImageRequest()
     
     public func loadGalleries(for page: Int, type: GalleryType, useViral: Bool, _ completion: @escaping ([GalleryModel]?) -> (Void)) {
         DispatchQueue.global(qos: .background).async {
             self.request.getGalleries(for: page, type: type, viral: useViral) { (result) in
-                let galleries = result?.map({ (gallery) in GalleryModel(gallery) })
-                completion(galleries)
+                
+                // request cover images info
+                let dispatchGroup = DispatchGroup()
+                for gallery in result ?? [] {
+                    if let coverId = gallery.coverImageId {
+                        dispatchGroup.enter()
+                        self.imageRequest.imageInfo(for: coverId, completion: { (cover) -> (Void) in
+                            gallery.coverImage = cover
+                            dispatchGroup.leave()
+                        })
+                    }
+                }
+                
+                dispatchGroup.notify(queue: .main) {
+                    let galleries = result?.map({ (gallery) in GalleryModel(gallery) })
+                    completion(galleries)
+                }                
             }
         }
     }
